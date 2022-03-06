f32347c039c6cf14976c851a549c966b59f210d5
manaemee <78135473+manaemee@users.noreply.github.com>
2022-03-06 11:54:20 +0900

    changed title

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme:DefaultTheme = {
+    bgandtextColor:"#DADADC",
+    boxColor:"#000000",
+    hoverColor:"#314353",
+}
+
+export const lightTheme:DefaultTheme = {
+    bgandtextColor:"#E3E6F5",
+    boxColor:"#000000",
+    hoverColor:"#2FDB9B",
+}
